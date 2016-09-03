97d3ad7d1b6e545d9d57f19f03fcc2611abedee7
yescallop <s@yescallop.cn>
2016-09-03 11:15:22 +0800

    Hmm..

--- src/main/java/cn/yescallop/essentialsnk/command/CommandBase.java
+++ src/main/java/cn/yescallop/essentialsnk/command/CommandBase.java
@@ -23,7 +23,7 @@ public abstract class CommandBase extends Command implements PluginIdentifiableC
     }
 
     @Override
-    protected EssentialsNK getPlugin() {
+    public EssentialsNK getPlugin() {
         return plugin;
     }
     
